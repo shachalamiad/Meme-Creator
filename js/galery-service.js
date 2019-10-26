c7d2e4f432783479e62e9d1e2d90e8bba6132053
Shachal Ami-Ad <shachalamiad@hotmail.com>
2019-10-26 15:23:20 +0300

    seperate picts/icons

--- js/galery-service.js
+++ js/galery-service.js
@@ -2,12 +2,12 @@
 
 const User_DATA_KEY_IMGS = 'Images';
 var gImgs = [
-    { id: 1, url: 'imgs/004.jpg', keywords: ['cute'] },
-    { id: 2, url: 'imgs/8.jpg', keywords: ['happy'] },
-    { id: 3, url: 'imgs/leo.jpg', keywords: ['cool'] },
-    { id: 4, url: 'imgs/img11.jpg', keywords: ['funny'] },
-    { id: 5, url: 'imgs/Oprah.jpg', keywords: ['funny'] },
-    { id: 6, url: 'imgs/2.jpg', keywords: ['beutiful'] }
+    { id: 1, url: 'imgs/pict/004.jpg', keywords: ['cute'] },
+    { id: 2, url: 'imgs/pict/8.jpg', keywords: ['happy'] },
+    { id: 3, url: 'imgs/pict/leo.jpg', keywords: ['cool'] },
+    { id: 4, url: 'imgs/pict/img11.jpg', keywords: ['funny'] },
+    { id: 5, url: 'imgs/pict/Oprah.jpg', keywords: ['funny'] },
+    { id: 6, url: 'imgs/pict/2.jpg', keywords: ['beutiful'] }
 ];
 
 saveImgsToStorage()
